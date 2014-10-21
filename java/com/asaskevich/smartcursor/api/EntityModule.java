be22125252e35cee565c23a2d0972926b431b436
Alex Saskevich <alexhomel@ya.ru>
2014-10-21 23:53:55 +0300

    Removed old and unused files

--- java/com/asaskevich/smartcursor/api/EntityModule.java
+++
@@ -1,8 +0,0 @@
-package com.asaskevich.smartcursor.api;
-
-import java.util.List;
-import net.minecraft.entity.Entity;
-
-public abstract class EntityModule {
-	abstract public void process(List<String> list, Entity entity);
-}
